7262f5a50ec75db6f76a4e2ed20988bc89f9245a
Fran <frankarroyop@gmail.com>
2022-02-06 00:34:11 +0100

    Creo formulario register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './control-acceso/login/login.component';
+import { RegisterComponent } from './control-acceso/register/register.component';
 import { DashboardComponent } from './home/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path:'',
-    component:DashboardComponent
+    component:DashboardComponent,canActivate:[AuthGuard]
   },
-  // {
-  //   path:'login',
-  //   loadChildren:() => import('./control-acceso/control-acceso.module').then(m=> m.ControlAccesoModule)
-  // },
-  {
-    path:'login',
-    component:LoginComponent
-  },
+   {
+     path:'login',
+     component:LoginComponent,
+     loadChildren:() => import('./control-acceso/control-acceso.module').then(m=> m.ControlAccesoModule)
+   },
+   {
+     path:'register',
+     component:RegisterComponent
+   },
   {
     path:'home',
     component:DashboardComponent,canActivate:[AuthGuard]
