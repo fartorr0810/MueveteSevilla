fc266423319fa641c342b350f6677d26e4d1fd03
Fran <frankarroyop@gmail.com>
2022-02-06 21:52:00 +0100

    Bueh

--- src/app/control-acceso/register/register.component.ts
+++ src/app/control-acceso/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,14 +10,22 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   formGroup!:FormGroup;
-
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
-  private buildForm(){
-    this.formGroup=this.formBuilder.group({})
+  buildForm(){
+    this.formGroup=this.formBuilder.group({
+      name:[Validators.required,Validators.minLength(4)],
+      username:[Validators.required,Validators.minLength(4),Validators.pattern("/^[a-zA-Z0-9]+$/")],
+      email:[Validators.required,Validators.email],
+      password:[Validators.required,Validators.minLength(6),Validators.pattern("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})")]
+    })
   }
-
+  register(){
+    const user=this.formGroup.value;
+    console.log(user);
+  }
 }
