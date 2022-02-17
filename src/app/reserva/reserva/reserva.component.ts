3730ab66984a1e2343834279c46c421461b40cc2
Fran <frankarroyop@gmail.com>
2022-02-17 10:00:12 +0100

    el prixmo commit si lo hago en la rama que toca perdon

--- src/app/reserva/reserva/reserva.component.ts
+++ src/app/reserva/reserva/reserva.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ControlAccesoService } from 'src/app/control-acceso/services/control-acceso.service';
+import { Patinete } from 'src/app/listar-patinete/interfaces/patinete.interface';
+import Swal from 'sweetalert2';
 import { AlquilerService } from '../services/alquiler.service';
 
 @Component({
@@ -12,14 +14,19 @@ import { AlquilerService } from '../services/alquiler.service';
 })
 export class ReservaComponent implements OnInit {
   formularioContacto!:FormGroup;
+  listapatinetesDisponibles: Patinete[] = [];
+  seleccion!:number;
+
 
   constructor(private router: Router,
     private authservice: ControlAccesoService,
     private fb:FormBuilder,
     private servicioContacto: AlquilerService) { }
 
-  ngOnInit(): void {
-
+  async ngOnInit() {
+    this.buildForm();
+    this.patinetesDisponibles();
+    this.authservice.obtenerUser();
   }
 
   buildForm(){
@@ -27,8 +34,9 @@ export class ReservaComponent implements OnInit {
     this.formularioContacto=this.fb.group({
       patinete:['',[Validators.required,Validators.min(0)]],
       horaentrega:['',[Validators.required]],
-      horasalquiler:['',[Validators.minLength(9)]],
-      confirmacion:['',[Validators.required]]
+      horasalquiler:['',[Validators.required,Validators.min(0)]],
+      confirmacion:[false,[Validators.required]],
+      user:[localStorage.getItem("idusuario"),],
     })
   }
 
@@ -37,9 +45,33 @@ export class ReservaComponent implements OnInit {
             && this.formularioContacto.controls[campo].touched;
   }
   calcularPrecioYFecha(){
-
+    const alquiler=this.formularioContacto.value;
+    console.log(alquiler.patinete);
+  }
+  patinetesDisponibles(){
+    this.servicioContacto.obtenerPatinetesDisponibles().subscribe({
+      next:(resp)=>{
+        this.listapatinetesDisponibles=resp;
+      },
+      error:()=>{
+        Swal.fire({
+          title: 'Lo sentimos',
+          text: 'No hay patinetes disponibles',
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        })
+      }
+    });
   }
   alquilar(){
+    const alquiler=this.formularioContacto.value;
+    console.log(alquiler);
+    this.servicioContacto.alquilarPatinete(alquiler).subscribe({
+    next:(resp=>{
+    }),
+    error:resp=>{
 
+    }
+  })
   }
 }
--- src/app/reserva/services/alquiler.service.ts
+++ src/app/reserva/services/alquiler.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Patinete } from 'src/app/listar-patinete/interfaces/patinete.interface';
 import { AlquilerI } from '../interfaces/alquiler.interface';
 
 @Injectable({
@@ -22,5 +24,10 @@ export class AlquilerService {
 
     const httpHeaders=new HttpHeaders();
     return this.http.post<AlquilerI>(direccionurl,bodypeticion,{ headers: httpHeaders});
+  }
+  obtenerPatinetesDisponibles():Observable<Patinete[]>{
+    let direccionurl="http://localhost:9000/patinete?filtro=disponible";
+    const httpHeaders=new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    return this.http.get<Patinete[]>(direccionurl,{ headers: httpHeaders});
   }
 }
